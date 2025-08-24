4e02d81a7495b07161acbc1defc997bc13e6dca4
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2025-08-25 03:06:48 +0530

    adds orderRepository with dep injection which injects SqlRepo and then this is used by order controller.

--- SOLID/ex09/src/Demo09.java
+++ SOLID/ex09/src/Demo09.java
@@ -1,5 +1,7 @@
 public class Demo09 {
     public static void main(String[] args) {
-        new OrderController().create("ORD-1");
+        OrderRepository sqlRepo = new SqlOrderRepository();
+        OrderController sqlController = new OrderController(sqlRepo);
+        sqlController.create("ORD-1");
     }
 }
