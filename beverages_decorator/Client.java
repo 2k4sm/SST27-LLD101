888587e5b92099ac573b98595ecfc6b7dddfdb28
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2025-09-15 18:04:50 +0530

    adds abstract decorator BeverageDecorator.

--- beverages_decorator/Client.java
+++ beverages_decorator/Client.java
@@ -2,20 +2,13 @@ package beverages_decorator;
 
 public class Client {
 
-	public static void main(String[] args) {
-		
-
-		
-		Beverage coffee = new Cappuccino();
+    public static void main(String[] args) {
+        Beverage coffee = new Cappuccino();
         System.out.println("Coffee");
-		System.out.println(coffee.cost());
-        coffee = new ChocolateDecorator(coffee);
         System.out.println(coffee.cost());
         coffee = new ChocolateDecorator(coffee);
         System.out.println(coffee.cost());
-		
-		
-
-	}
-
+        coffee = new ChocolateDecorator(coffee);
+        System.out.println(coffee.cost());
+    }
 }
